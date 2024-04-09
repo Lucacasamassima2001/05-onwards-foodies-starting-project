a07adf0101e4cae74783928d04633c8ca14a8170
Luca Casamassima <casamassimaluca723@gmail.com>
2024-04-09 17:44:53 +0200

    added navigation and details page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,36 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
 import Link from "next/link";
-export default function meals() {
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/ciao">Slug page</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals , created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          {" "}
+          Choose your fevourite recpies and cook it yourself. It is easy and
+          free
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite Recipe </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
